d82b161502e40224c1379a65d0e4b9498946200c
kjmahalingam <dylan@mahalingam.com>
2024-08-16 16:01:15 -0400

    Add tween for size change

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -5,6 +5,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 @export var BIG_SIZE_SCALE = 1.5
 @export var SMALL_SIZE_SCALE = 0.5
+@export var SIZE_CHANGE_DURATION = 0.1
 
 var BIG_SCALE_VEC = Vector2(BIG_SIZE_SCALE, BIG_SIZE_SCALE)
 var SMALL_SCALE_VEC = Vector2(SMALL_SIZE_SCALE, SMALL_SIZE_SCALE)
@@ -12,10 +13,12 @@ var SMALL_SCALE_VEC = Vector2(SMALL_SIZE_SCALE, SMALL_SIZE_SCALE)
 func _physics_process(delta: float) -> void:
 		
 	if Input.is_action_just_pressed("scale_toggle"):
+		var tween = create_tween()
+
 		if scale == SMALL_SCALE_VEC:
-			scale = BIG_SCALE_VEC
+			tween.tween_property(self, "scale", BIG_SCALE_VEC, SIZE_CHANGE_DURATION)
 		else:
-			scale = SMALL_SCALE_VEC
+			tween.tween_property(self, "scale", SMALL_SCALE_VEC, SIZE_CHANGE_DURATION)
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("left", "right")
