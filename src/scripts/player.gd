22554a7980139f78bbb13289d8391d1a94d0f5bd
mgramigna <matthewgramigna@gmail.com>
2024-08-16 15:47:46 -0400

    chore: remove gravity and use vector constants

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -6,17 +6,16 @@ const JUMP_VELOCITY = -400.0
 @export var BIG_SIZE_SCALE = 1.5
 @export var SMALL_SIZE_SCALE = 0.5
 
+var BIG_SCALE_VEC = Vector2(BIG_SIZE_SCALE, BIG_SIZE_SCALE)
+var SMALL_SCALE_VEC = Vector2(SMALL_SIZE_SCALE, SMALL_SIZE_SCALE)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 		
 	if Input.is_action_just_pressed("scale_toggle"):
-		if scale == Vector2(SMALL_SIZE_SCALE, SMALL_SIZE_SCALE):
-			scale = Vector2(BIG_SIZE_SCALE, BIG_SIZE_SCALE)
+		if scale == SMALL_SCALE_VEC:
+			scale = BIG_SCALE_VEC
 		else:
-			scale = Vector2(SMALL_SIZE_SCALE, SMALL_SIZE_SCALE)
+			scale = SMALL_SCALE_VEC
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("left", "right")
