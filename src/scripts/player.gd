39b136e8ad283b7f40c8726d788e18cb62e71605
mgramigna <matthewgramigna@gmail.com>
2024-08-16 15:41:59 -0400

    feat: toggle between two scale sizes

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -3,23 +3,20 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-@export var SCALE_MULTIPLIER = 1.1
+@export var BIG_SIZE_SCALE = 1.5
+@export var SMALL_SIZE_SCALE = 0.5
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 		
-	if Input.is_action_pressed("scale_up"):
-		scale *= SCALE_MULTIPLIER
-	
-	if Input.is_action_pressed("scale_down"):
-		scale /= SCALE_MULTIPLIER
+	if Input.is_action_just_pressed("scale_toggle"):
+		if scale == Vector2(SMALL_SIZE_SCALE, SMALL_SIZE_SCALE):
+			scale = Vector2(BIG_SIZE_SCALE, BIG_SIZE_SCALE)
+		else:
+			scale = Vector2(SMALL_SIZE_SCALE, SMALL_SIZE_SCALE)
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("left", "right")
