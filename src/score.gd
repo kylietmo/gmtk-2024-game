64563cd20358c8cdd0453926f6882087b9eae6b8
mgramigna <matthewgramigna@gmail.com>
2024-08-17 16:43:20 -0400

    fix: score label pos

--- src/score.gd
+++ src/score.gd
@@ -4,6 +4,7 @@ const SCORE_LABEL_OFFSET = 50
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	position.x = Globals.LEFT_BARRIER_X + SCORE_LABEL_OFFSET
 	position.y = -Globals.IN_BOUNDS_HEIGHT/2 + SCORE_LABEL_OFFSET
 	Globals.connect("score_changed", _on_score_changed)
 	
