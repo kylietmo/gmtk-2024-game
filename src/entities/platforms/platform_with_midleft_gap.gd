39c0f7cdd5e3a33f9a54029abd26a44c126be3d3
kjmahalingam <dylan@mahalingam.com>
2024-08-17 19:38:04 -0400

    Add midleft and midright gaps

---
+++ src/entities/platforms/platform_with_midleft_gap.gd
@@ -0,0 +1,24 @@
+extends PlatformRow
+
+@onready var left_platform: Obstacle = $LeftPlatform
+@onready var right_platform: Obstacle = $RightPlatform
+
+const GAP_WIDTH_SCALAR = .1
+
+# TODO: consider randomized height
+func _ready():
+	var gap_width = Globals.IN_BOUNDS_WIDTH * GAP_WIDTH_SCALAR
+	
+	var left_platform_width = (1.0 - GAP_WIDTH_SCALAR) / 2 * Globals.IN_BOUNDS_WIDTH - (Globals.IN_BOUNDS_WIDTH / 4)
+	var right_platform_width = (1.0 - GAP_WIDTH_SCALAR) / 2 * Globals.IN_BOUNDS_WIDTH + (Globals.IN_BOUNDS_WIDTH / 4)
+
+	var sprite : Sprite2D = left_platform.find_child("Sprite2D")
+
+	var platform_sprite_width = sprite.texture.get_size().x
+	var platform_sprite_height = sprite.texture.get_size().y
+	
+	left_platform.position.x = Globals.LEFT_BARRIER_X + left_platform_width / 2
+	right_platform.position.x = Globals.RIGHT_BARRIER_X - right_platform_width / 2
+
+	left_platform.scale.x = left_platform_width / platform_sprite_width
+	right_platform.scale.x = right_platform_width / platform_sprite_width
