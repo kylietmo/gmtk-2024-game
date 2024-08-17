6fd3e7acaa685473cc5dfcde2b947f0f9a0d9963
mgramigna <matthewgramigna@gmail.com>
2024-08-17 16:47:24 -0400

    chore: center menu elements

--- src/menus/game_over_screen/game_over_screen.gd
+++ src/menus/game_over_screen/game_over_screen.gd
@@ -2,6 +2,10 @@ extends Control
 
 func _ready() -> void:
 	$FinalScore.text = "Final Score: " + str(Globals.score)
+	$FinalScore.position.x = Globals.SCREEN_WIDTH / 2 - $FinalScore.get_rect().size.x / 2
+	$Label.position.x = Globals.SCREEN_WIDTH / 2 - $Label.get_rect().size.x / 2
+	$TryAgainButton.position.x = Globals.SCREEN_WIDTH / 2 - $TryAgainButton.size.x / 2
+	$MainMenuButton.position.x = Globals.SCREEN_WIDTH / 2 - $MainMenuButton.size.x / 2
 
 func _on_try_again_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
--- src/menus/start_screen/start_screen.gd
+++ src/menus/start_screen/start_screen.gd
@@ -7,3 +7,8 @@ func _on_start_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+func _ready() -> void:
+	$Label.position.x = Globals.SCREEN_WIDTH / 2 - $Label.get_rect().size.x / 2
+	$StartButton.position.x = Globals.SCREEN_WIDTH / 2 - $StartButton.size.x / 2
+	$QuitButton.position.x = Globals.SCREEN_WIDTH / 2 - $QuitButton.size.x / 2
