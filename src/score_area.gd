228e55c47a98adacb7c6914fc539d5beab5b9daf
mgramigna <matthewgramigna@gmail.com>
2024-08-17 11:55:54 -0400

    fix: offset score collider

--- src/score_area.gd
+++ src/score_area.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
-@onready var player: Player = %Player
+@onready var player : Player = %Player
 
-func _ready() -> void:
-	position.y = player.position.y
+const SCORE_COLLIDER_OFFSET = 40
+
+func _ready() -> void:	
+	position.y = player.position.y  - SCORE_COLLIDER_OFFSET
