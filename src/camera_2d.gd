e82b474a32bea1ee584204e3f6d6c793224a923e
mgramigna <matthewgramigna@gmail.com>
2024-08-18 17:55:08 -0400

    feat: move camera when changing sizes

--- src/camera_2d.gd
+++ src/camera_2d.gd
@@ -4,12 +4,25 @@ extends Camera2D
 @onready var LARGE_SHAKE_STRENGTH = Globals.IN_BOUNDS_HEIGHT / 20
 @onready var SHAKE_FADE = Globals.IN_BOUNDS_HEIGHT / 200
 
+@onready var player : Player = %Player
+
 var shake_strength = 0.0
+var camera_y_offset = Globals.IN_BOUNDS_HEIGHT / 15
+var starting_y_position : float
+
+func _ready() -> void:
+	starting_y_position = position.y
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("size_small"):
+	if Input.is_action_just_pressed("size_small") and player.current_size != player.sizes.SMALL and player.current_size != player.sizes.MASSIVE:
 		apply_small_shake()
+		position.y -= camera_y_offset
+		$CameraResetTimer.start(player.SMALL_SIZE_DURATION + 2*player.SIZE_TRANSITION_DURATION)
+		
+	if Input.is_action_just_pressed("size_large") and player.current_size != player.sizes.LARGE and player.current_size != player.sizes.MASSIVE:
+		position.y += camera_y_offset
+		$CameraResetTimer.start(player.LARGE_SIZE_DURATION + 2*player.SIZE_TRANSITION_DURATION)
 
 	if shake_strength > 0:
 		shake_strength = lerpf(shake_strength, 0, SHAKE_FADE * delta)
@@ -26,3 +39,7 @@ func random_offset() -> Vector2:
 		randf_range(-shake_strength, shake_strength),
 		randf_range(-shake_strength, shake_strength)
 	)
+
+
+func _on_camera_reset_timer_timeout() -> void:
+	position.y = starting_y_position
