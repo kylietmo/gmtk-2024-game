296f937f39eea55a0bae564c4781f0b1dabed84c
kjmahalingam <dylan@mahalingam.com>
2024-08-18 12:00:34 -0400

    Add speed up sound

---
+++ src/speed_up_sound.gd
@@ -0,0 +1,6 @@
+extends AudioStreamPlayer2D
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("size_small"):
+		play()
