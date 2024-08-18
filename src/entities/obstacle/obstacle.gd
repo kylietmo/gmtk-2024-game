866027ec68ee542f2c100dc1308431df24ee1a20
mgramigna <matthewgramigna@gmail.com>
2024-08-18 16:52:12 -0400

    fix: camera shake despawning bug

--- src/entities/obstacle/obstacle.gd
+++ src/entities/obstacle/obstacle.gd
@@ -2,5 +2,14 @@ extends CharacterBody2D
 class_name Obstacle
 signal broke_platform
 
+@export var DESPAWN_TIME = 1.0
+
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	queue_free()
+	# only free if exiting from the top of the screen
+	# TODO: can we do this without the parent?
+	if get_parent().position.y < 0:
+		$DespawnTimer.start(DESPAWN_TIME)
+
+
+func _on_timer_timeout() -> void:
+	queue_free.call_deferred()
--- src/entities/platforms/platform_row.gd
+++ src/entities/platforms/platform_row.gd
@@ -8,6 +8,7 @@ const SPAWN_THRESHOLD_Y = 50
 var has_passed_score_threshold = false
 var has_passed_spawn_threshold = false
 
+
 func _process(_delta: float) -> void:
 	if not has_passed_score_threshold and position.y < Globals.PLAYER_START_Y:
 		passed_score_threshold.emit()
