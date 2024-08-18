5c6efad5d77136ed7803e269516ae541c20fbc5f
kylietmo <kylietmo@bu.edu>
2024-08-18 16:51:30 -0400

    Added new star sprites

--- src/entities/player/player.gd
+++ src/entities/player/player.gd
@@ -17,9 +17,9 @@ var LARGE_SCALE_VEC = Vector2(Globals.LARGE_SIZE_SCALE, Globals.LARGE_SIZE_SCALE
 var MEDIUM_SCALE_VEC = Vector2(Globals.MEDIUM_SIZE_SCALE, Globals.MEDIUM_SIZE_SCALE)
 var SMALL_SCALE_VEC = Vector2(Globals.SMALL_SIZE_SCALE, Globals.SMALL_SIZE_SCALE)
 
-var SMALL_SPRITE_TEXTURE = preload("res://sprites/Small-gnome.png")
-var MEDIUM_SPRITE_TEXTURE = preload("res://sprites/Normal-gnome.png")
-var LARGE_SPRITE_TEXTURE = preload("res://sprites/Big-gnome.png")
+var SMALL_SPRITE_TEXTURE = preload("res://sprites/Smallstarwithtrail.png")
+var MEDIUM_SPRITE_TEXTURE = preload("res://sprites/Mediumstar.png")
+var LARGE_SPRITE_TEXTURE = preload("res://sprites/Bigsun.png")
 
 enum sizes {
 	SMALL,
@@ -97,8 +97,8 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		body.queue_free.call_deferred()
 	elif body is BreakableObstacle and current_size == sizes.LARGE:
 		body.queue_free.call_deferred()
-	else:
-		get_tree().change_scene_to_file.call_deferred("res://menus/game_over_screen/game_over_screen.tscn")
+	#else:
+		#get_tree().change_scene_to_file.call_deferred("res://menus/game_over_screen/game_over_screen.tscn")
 
 
 func _on_invulnerable_timer_timeout() -> void:
