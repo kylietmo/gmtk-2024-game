229e13c7d61912001fb9a61f212771061e5f6c19
kjmahalingam <dylan@mahalingam.com>
2024-08-19 20:31:59 -0400

    Add how to play

---
+++ src/menus/how_to_play_screen/how_to_play_screen.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+func _ready() -> void:
+	$VBoxContainer/BackButton.grab_focus()
+	$ConsumableSprite.play()
+
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://menus/start_screen/start_screen.tscn")
--- src/menus/start_screen/start_screen.gd
+++ src/menus/start_screen/start_screen.gd
@@ -5,3 +5,7 @@ func _ready() -> void:
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://game.tscn")
+
+
+func _on_how_to_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://menus/how_to_play_screen/how_to_play_screen.tscn")
